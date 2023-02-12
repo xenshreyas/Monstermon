88baa27e8b6e9a956b9396925e69366bc48bae86
Shreyas <xenshreyas@gmail.com>
2023-02-12 13:21:42 -0800

    Added class level comments for exceptions

--- src/main/ui/exceptions/InvalidNumberOfHealthPointsException.java
+++ src/main/ui/exceptions/InvalidNumberOfHealthPointsException.java
@@ -1,4 +1,5 @@
 package ui.exceptions;
 
+// Represents an exception that is thrown when an invalid number of health points is entered by user
 public class InvalidNumberOfHealthPointsException extends Exception {
 }
--- src/main/ui/exceptions/InvalidTypeException.java
+++ src/main/ui/exceptions/InvalidTypeException.java
@@ -1,4 +1,5 @@
 package ui.exceptions;
 
+// Represents an exception that is thrown when the user enters a type that is not valid
 public class InvalidTypeException extends Exception {
 }
